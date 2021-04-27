d39c8feee1c95f3199f81f340538ed5803976939
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-27 15:23:49 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - PROPERTIES/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCISE_01___PROPERTIES {
+    //------------------------------- START -------------------------------//
+    class Product {
+        private string _name;
+        private double _price;
+        private int _amount;
+        public Product() {
+        }
+        public Product(string name, double price, int amount) {
+            _name = name;
+            _price = price;
+            _amount = amount;
+        }
+        public string Name {
+            get { return _name; }
+            set { if (value != null && value.Length > 1) {
+                    _name = value;
+                }
+            }
+        }       
+        public double Price {
+            get { return _price; }
+        }
+        public int Amount {
+            get { return _amount; }
+        }
+        
+        public double AmountValueStock() {
+            return _price * _amount;
+        }
+        public void AddProducts(int quantidade) {
+            _amount += quantidade;
+        }
+        public void RemoveProducts(int quantidade) {
+            _amount -= quantidade;
+        }
+        public override string ToString() {
+            return _name
+            + ", $ " + _price.ToString("F2") + ", " + _amount + " unidades, Total: $ " + AmountValueStock().ToString("F2");
+        }
+    }
+    //-------------------------------- END -------------------------------// 
+}
---
+++ EXERCISE 01 - PROPERTIES/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace EXERCISE_01___PROPERTIES {
+    class Program {
+        //-----> START
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        static void Main(string[] args) {
+                Product p = new Product("tv", 500, 10);
+            p.Name = "TV 4K";
+            
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.Price);
+            }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        //-----> END
+    }
+}
